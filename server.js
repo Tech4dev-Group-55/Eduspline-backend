69f71ad168b349cdfa62325e756cec4efb0db1b2
Adaobi Okwuosa <adaobi@wellahealth.com>
2026-02-23 18:52:55 +0100

    fix render

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+process.env.UV_THREADPOOL_SIZE = 128;
+require('dns').setDefaultResultOrder('ipv4first');
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
